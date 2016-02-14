f2713f200d38a2d188d06ca01e4e4c161fba4d45
wenshaomin <winshaomin@techshino.com>
2016-02-14 11:36:24 +0800

    添加了一个service

--- src/com/albert/ifttt/MainActivity.java
+++ src/com/albert/ifttt/MainActivity.java
@@ -1,44 +1,56 @@
 package com.albert.ifttt;
 
+import com.albert.ifttt.R;
+import com.albert.ifttt.R.id;
+import com.albert.ifttt.R.layout;
+import com.albert.ifttt.R.menu;
+import com.albert.ifttt.service.MainService;
+
 import android.app.Activity;
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.os.Build;
 
-
-
 public class MainActivity extends Activity {
+	private Intent intent = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		intent = new Intent(MainActivity.this, MainService.class);
 
+		
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment(intent)).commit();
+		}
+		
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- src/com/albert/ifttt/PlaceholderFragment.java
+++ src/com/albert/ifttt/PlaceholderFragment.java
@@ -1,23 +1,57 @@
 package com.albert.ifttt;
 
+import com.albert.ifttt.service.MainService;
+
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+	private Button startButton = null;
+	private Button stopButton = null;
+	private Intent intent = null;
 
-    public PlaceholderFragment() {
-    }
+	public PlaceholderFragment() {
+		
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        return rootView;
-    }
+	public PlaceholderFragment(Intent intent) {
+		// TODO Auto-generated constructor stub
+		this.intent = intent;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main, container,
+				false);
+		startButton = (Button) rootView.findViewById(R.id.bt_start);
+		stopButton = (Button) rootView.findViewById(R.id.bt_stop);
+		startButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				getActivity().startService(intent);
+			}
+		});
+
+		// set stop service button click listener
+		stopButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				getActivity().stopService(intent);
+			}
+		});
+		return rootView;
+	}
 }
